edf1aec11ebfdb0e7093c3134b0a9e7793229b4b
JaydenHollissSAE <78131640+JKCrafted@users.noreply.github.com>
2026-04-17 22:27:55 +1000

    Update variables.py

--- variables.py
+++ variables.py
@@ -1,9 +1,19 @@
 string_demo = "This is a string"
 
+print(string_demo, type(string_demo))
+
 int_demo = 52
+
+print(int_demo, type(int_demo))
 
 float_demo = 653.83409
 
+print(float_demo, type(float_demo))
+
 list_demo = [1,2,3,56] # Can edit
 
+print(list_demo, type(list_demo))
+
 tuple_demo = (1,2,3,56) # Can't edit
+
+print(tuple_demo, type(tuple_demo))
